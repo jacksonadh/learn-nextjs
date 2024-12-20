2347614362de1bbffc509a13a6aed4595893e82d
Jackson Almeida <jacksonadh@gmail.com>
2024-12-20 14:12:55 -0300

    lint fix erro deploy

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -3,7 +3,7 @@ import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
 import { fetchCustomers, fetchInvoiceById } from '@/app/lib/data';
 import { notFound } from 'next/navigation';
 
-export default async function (props: { params: Promise<{ id: string }> }) {
+export default async function EditInvoice(props: { params: Promise<{ id: string }> }) {
  const params = await props.params;
  const id = params.id;
 
--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -31,7 +31,7 @@ export type State = {
 };
 
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
-export async function createInvoice(prevState: State, formData: FormData) {
+export async function createInvoice(formData: FormData) {
   const validatedFields = CreateInvoice.safeParse({
     customerId: formData.get("customerId"),
     amount: formData.get("amount"),
@@ -53,7 +53,7 @@ export async function createInvoice(prevState: State, formData: FormData) {
       INSERT INTO invoices (customer_id, amount, status, date)
       VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
     `;
-  } catch (error) {
+  } catch {
     return {
       message: "Database Error: Failed to Create Invoice.",
     };
@@ -64,11 +64,7 @@ export async function createInvoice(prevState: State, formData: FormData) {
 }
 
 const UpdateInvoice = FormSchema.omit({ id: true, date: true });
-export async function updateInvoice(
-  id: string,
-  prevState: State,
-  formData: FormData
-) {
+export async function updateInvoice(id: string, formData: FormData) {
   const validatedFields = UpdateInvoice.safeParse({
     customerId: formData.get("customerId"),
     amount: formData.get("amount"),
@@ -91,7 +87,7 @@ export async function updateInvoice(
       SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
       WHERE id = ${id}
     `;
-  } catch (error) {
+  } catch {
     return { message: "Database Error: Failed to Update Invoice." };
   }
 
@@ -103,7 +99,7 @@ export async function deleteInvoice(id: string) {
     await sql`DELETE FROM invoices WHERE id = ${id}`;
     revalidatePath("/dashboard/invoices");
     return { message: "Deleted Invoice." };
-  } catch (error) {
+  } catch {
     return { message: "Database Error: Failed to Delete Invoice." };
   }
 }
